b7b055c863017d599ab59357061ef627925e3503
amudalasailasya1109-sudo <amudalasailasya1109@gmail.com>
2026-04-05 14:23:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 
-// app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
